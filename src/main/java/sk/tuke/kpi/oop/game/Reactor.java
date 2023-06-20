75ca574fae0ada194209ac95da1e8e5994ffc08c
elkalinova <kalinova.elena@gmail.com>
2023-06-20 13:15:38 +0200

    uprava damage reaktora nejde

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -9,24 +9,34 @@ public class Reactor extends AbstractActor {
     private int damage;
     private Animation normalAnimation;
     public Reactor() {
-        temperature = 0;
-        state = false;
-        damage = 0;
+        this.temperature = 0;
+        this.state = false;
+        this.damage = 0;
         normalAnimation = new Animation("sprites/reactor_on.png", 80,80, 0.1F, Animation.PlayMode.LOOP_PINGPONG);
         setAnimation(normalAnimation);
     }
     public int getTemperature()
-        { return temperature;}
+        { return this.temperature;}
     public int getDamage()
-    { return damage;}
+    { return this.damage;}
 
     public void increaseTemperature(int increment)
-    { temperature += increment;
-        if (temperature=>2000 && temperature=<6000)
-            int damage = temperature/40 - 50;
-        if (damage < damage)
-         damage = damage;
-
+    { this.temperature += increment;
+        if (this.temperature>=2000) {
+            if (this.temperature <= 6000) {
+                this.damage = 100;
+                return;
+            } else
+                int damage = this.temperature / 40 - 50;
+            if (this.damage < damage)
+                this.damage = damage;
+        }
+        if (this.temperature >= 4000){
+            setAnimation(
+                new Animation("sprites/reactor_hot.png", 80,80, 0.1F, Animation.PlayMode.LOOP_PINGPONG);
+            );
+            setAnimation(normalAnimation);
+        }
 
     }
 }
