2ac2c4eefd683e1e41b31f162e503bc32b93a0e7
elkalinova <kalinova.elena@gmail.com>
2023-06-22 11:03:03 +0200

    pridany Controller pre reactor

--- src/main/java/sk/tuke/kpi/oop/game/Controller.java
+++ src/main/java/sk/tuke/kpi/oop/game/Controller.java
@@ -1,4 +1,14 @@
 package sk.tuke.kpi.oop.game;
 
-public class Controller {
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class Controller extends AbstractActor {
+    private Reactor reactor;
+
+    public Controller (Reactor reactor) {
+        this.reactor = reactor;
+        Animation animation = new Animation("sprites/switch.png");
+        setAnimation(animation);
+    }
 }
